e3b10e82e082d7254ac61d348a7036363ac0fe96
D-Brox <davidfbg00@gmail.com>
2025-04-23 00:22:29 -0300

    fix: only mark manual cosmic-session and recommended

--- src/main.rs
+++ src/main.rs
@@ -43,14 +43,15 @@ async fn main() {
         "xdg-desktop-portal-cosmic",
     ]);
 
-    let arch= if cfg!(target_arch = "x86_64") {
+    let arch = if cfg!(target_arch = "x86_64") {
         "amd64"
     } else if cfg!(target_arch = "aarch64") {
         "arm64"
     } else {
         panic!("Architecture not suported");
     };
-    let url = format!("https://apt-origin.pop-os.org/release/dists/noble/main/binary-{arch}/Packages");
+    let url =
+        format!("https://apt-origin.pop-os.org/release/dists/noble/main/binary-{arch}/Packages");
 
     let data = get(url)
         .await
@@ -60,16 +61,13 @@ async fn main() {
         .expect("Failed to read Packages file");
 
     let packages = Packages::from(&data);
-    // let tempdir = TempDir::new("install_cosmic").expect("Failed to create tempdir");
-
-    let tempdir = Path::new("/tmp/install_cosmic");
-    std::fs::create_dir(tempdir).expect("Failed to create dir");
+    let tempdir = TempDir::new("install_cosmic").expect("Failed to create tempdir");
     let mut urls = vec![];
     let pb = ProgressBar::new(cosmic_packages.len() as u64);
     pb.inc(0);
     for package in packages {
         if cosmic_packages.contains(package.package.as_str()) {
-            let file = tempdir.join(format!("{}.deb", package.package));
+            let file = tempdir.path().join(format!("{}.deb", package.package));
             urls.push(file.to_str().unwrap().to_string());
             std::fs::write(
                 file,
@@ -88,27 +86,32 @@ async fn main() {
         }
     }
 
-    let file = tempdir.join("libdisplay-info1.deb");
+    let file = tempdir.path().join("libdisplay-info1.deb");
     urls.push(file.to_str().unwrap().to_string());
     let req = get(format!("https://launchpad.net/ubuntu/+archive/primary/+files/libdisplay-info1_0.1.1-2build1_{arch}.deb"));
     let client = surf::client().with(surf::middleware::Redirect::new(5));
     std::fs::write(
         file,
-        client.send(req)
-        .await
-        .expect("Failed to get deb")
-        .body_bytes()
-        .await
-        .expect("Failed to read deb"),
+        client
+            .send(req)
+            .await
+            .expect("Failed to get deb")
+            .body_bytes()
+            .await
+            .expect("Failed to read deb"),
     )
     .expect("Failed to download deb");
 
     let cache = new_cache!(&urls).expect("Failed to load apt cache");
-    let pkg = cache.get("libdisplay-info1").expect("Failed to get package");
-    pkg.mark_install(true, true);
-    pkg.protect();
 
-    for package in cosmic_packages {
+    for package in [
+        "cosmic-session",
+        "cosmic-edit",
+        "cosmic-player",
+        "cosmic-store",
+        "cosmic-term",
+        "cosmic-wallpapers",
+    ] {
         let pkg = cache.get(&package).expect("Failed to get package");
         pkg.mark_install(true, true);
         pkg.protect();
